5d2e74c0888823abb5c0e443bcce7a789a2300d1
Kyle Crews <kyle@banteegolf.com>
2023-12-09 19:04:04 -0600

    updated index.html

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.getElementById('changeImageButton').addEventListener('click', function() {
     var displayedImage = document.getElementById('displayedImage');
-    displayedImage.src = 'another-image-url.jpg'; // Replace with new image URL
+    displayedImage.style.opacity = '0';
+
+    setTimeout(function() {
+        displayedImage.src = 'another-image-url.jpg'; // Replace with new image URL
+        displayedImage.style.opacity = '1';
+    }, 500);
 });
