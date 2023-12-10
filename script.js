b6619f536c520d497b9849e34c4e0ddf22c9dd7f
Kyle Crews <kyle@banteegolf.com>
2023-12-09 21:13:46 -0600

    updated index.html

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-import image from image.jpg
-
 document.getElementById('changeImageButton').addEventListener('click', function() {
     var displayedImage = document.getElementById('displayedImage');
     displayedImage.style.opacity = '0';
 
     setTimeout(function() {
-        displayedImage.src = image; // Replace with new image URL
+        displayedImage.src = 'image.jpg';
         displayedImage.style.opacity = '1';
     }, 500);
 });
